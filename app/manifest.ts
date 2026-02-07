828f625dd0847292f1fcc21cf6e0c9aae512ebe7
Michael <michael.de.bruyne@hotmail.be>
2026-02-07 09:36:25 +0100

    fix: finalize PWA manifest and icons

--- app/manifest.ts
+++ app/manifest.ts
@@ -4,27 +4,20 @@ export default function manifest(): MetadataRoute.Manifest {
   return {
     name: "Steuntje",
     short_name: "Steuntje",
-    description: "Dagelijkse emotionele EHBO in 20 seconden",
     start_url: "/",
     display: "standalone",
-    background_color: "#f8f1ea",
-    theme_color: "#f8f1ea",
+    background_color: "#fff8f1",
+    theme_color: "#fff8f1",
     icons: [
       {
-        src: "/icons/manifest-icon-192.png",
+        src: "/icons/icon-192.png",
         sizes: "192x192",
         type: "image/png",
       },
       {
-        src: "/icons/manifest-icon-512.png",
+        src: "/icons/icon-512.png",
         sizes: "512x512",
         type: "image/png",
-      },
-      {
-        src: "/icons/manifest-icon-512.png",
-        sizes: "512x512",
-        type: "image/png",
-        purpose: "maskable",
       },
     ],
   };
